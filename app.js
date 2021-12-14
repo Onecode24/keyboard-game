d2c8198367218c12a7e4cf7d2751eedb2aefd004
Onecode24 <88493737+Onecode24@users.noreply.github.com>
2021-12-14 15:59:31 +0100

    version_1

---
+++ app.js
@@ -0,0 +1,31 @@
+const allButton=document.querySelectorAll('button');
+const score=document.querySelector('.score');
+var val=0;
+console.log(allButton);
+function makeRandom(){
+    var num=Math.floor(Math.random()*allButton.length)
+    return num;
+}
+ function boucle(){
+    placeScore(val);
+     var hold=makeRandom()
+ allButton[hold].classList.add('jiggle');
+ console.log(hold);
+ allButton[hold].addEventListener('click',()=>{
+     allButton[hold].classList.remove('jiggle');
+     boucle();
+     
+ })
+ val++;
+} 
+var one=1,two=2;  
+//boucle();
+if(two>one){
+    boucle();
+    one+=8;
+}
+
+function placeScore(num){
+    score.innerHTML=`${num}`;
+    console.log(score);
+}
